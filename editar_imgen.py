e4ed914a261fd2549b8b703045b255df461dafa6
Cerritos <manecerritos808@gmail.com>
2024-08-23 12:17:05 -0600

    Deteccion de bordes

--- editar_imgen.py
+++ editar_imgen.py
@@ -4,9 +4,9 @@ import numpy as nu
 img = cv2.imread("pai.jpg")
 cv2.imshow("Original", img)
 cv2.waitKey(0)
-imgGris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-"""imgGris =img
-h,w,a=img.shape
+#imgGris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+imgGris =img
+h,w,a=img.shape             
 mean = 0
 cont =0
 for i in range(h):
@@ -15,7 +15,7 @@ for i in range(h):
            k=img[i,j,z]
            imgGris[i,j]=k
            mean+=k
-           cont+=1"""
+           cont+=1
 cv2.imshow("Gris", imgGris)
 cv2.waitKey(0)
 imgNegativo = img
@@ -23,9 +23,10 @@ mean/=cont
 for i in range(h):
     for j in range(w):
         for z in range (a):
-            if imgGris[i,j]<mean:
-                imgNegativo[i,j,z]=255
-            else:
+            if imgGris[i,j,z]<mean:
                 imgNegativo[i,j,z]=0
+            else:
+                imgNegativo[i,j,z]=255
+
 cv2.imshow("separacion de claro y obscuro", imgNegativo)
 cv2.waitKey(0)
